ac3352992e7c6232226285f953dcf368f818dd4a
ntd1407 <ndat526686@gmail.com>
2024-12-16 20:23:32 +0700

    1

--- src/components/config/apiUrl/index.js
+++ src/components/config/apiUrl/index.js
@@ -1,5 +1,3 @@
-export const API_URL =
-        document.domain === 'localhost'
-        ? "https://web-btl-2023.onrender.com"
-        : "production";
+export const API_URL = "https://web-btl-2023.onrender.com"
+  
 
